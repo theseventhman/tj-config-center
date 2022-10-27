73bcccff2e7ecd1dc12c341399c3e7b1e3963096
tcn741 <tianchenlko@126.com>
2022-10-27 22:47:04 +0800

    提交文件

---
+++ src/main/java/com/tj/exercise/springframework/tjconfigcenter/TjConfigCenterApplication.java
@@ -0,0 +1,13 @@
+package com.tj.exercise.springframework.tjconfigcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjConfigCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tj/exercise/springframework/tjconfigcenter/TjConfigCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.exercise.springframework.tjconfigcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjConfigCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
